270d4e310d1238c6ede914b130f8cba9bc178a3b
HeadTriXz <l.knol@outlook.com>
2024-03-03 18:43:50 +0100

    feat: use relative parameters

--- src/calculate_distance/main.py
+++ src/calculate_distance/main.py
@@ -3,24 +3,24 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 """Calibrated parameters"""
-a0 = 325
-b0 = 0.826
-c0 = 386
-d0 = -1.475
+a0 = 1.015988
+b0 = 0.825809
+c0 = 0.357420
+d0 = -1.46350
 
 """Functions"""
 def formula(x, a, b, c, d):
     """The formula to fit the data to."""
     return a / (x - c) ** b + d
 
-def recalibrate_parameters(x, y) -> list[float]:
+def recalibrate_parameters(x: np.ndarray, y: np.ndarray) -> (float, float, float, float):
     """Recalibrates the parameters of the formula to fit the given data."""
-    popt, pcov = curve_fit(formula, x, y, p0=[a0, b0, c0, d0])
-    return popt
+    fit = curve_fit(formula, x, y, p0=[a0, b0, c0, d0])
+    return fit[0]
 
-def plot_data(x, y, popt):
+def plot_data(x: np.ndarray, y: np.ndarray, popt: (float, float, float, float)) -> None:
     """Plots the given data and the fitted line."""
-    x_reg = np.linspace(430, 1080, 100)
+    x_reg = np.linspace(0.4, 1, 1000)
 
     plt.plot(x, y, 'ro', label='Original data')
     plt.plot(x_reg, formula(x_reg, *popt), 'b-', label='Fitted line')
@@ -28,35 +28,37 @@ def plot_data(x, y, popt):
 
     plt.show()
 
-def y_to_meters(y: int) -> float:
+def y_to_meters(y: int, height: int) -> float:
     """Converts the y-value to meters."""
-    if y <= c0:
+    rel_y = y / height
+    if rel_y <= c0:
         raise ValueError("The y-value is too low.")
 
-    return formula(y, a0, b0, c0, d0)
+    return formula(rel_y, a0, b0, c0, d0)
 
-def meters_to_y(meters: float) -> int:
+def meters_to_y(meters: float, height: int) -> int:
     """Converts the meters value to y-value."""
-    return int((a0 / (meters - d0)) ** (1 / b0) + c0)
-
+    rel_y = ((a0 / (meters - d0)) ** (1 / b0) + c0)
+    return int(round(rel_y * height))
 
 if __name__ == '__main__':
-    X = np.array([1028, 840, 733, 621, 561, 523, 499, 481, 468, 457, 449, 443])
-    Y = np.array([0.1, 0.6, 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 8.1, 9.1, 10.1])
+    X = np.array([1028, 840, 733, 621, 561, 523, 499, 481, 468, 457, 449, 443]) / 1080
+    Y = np.array([0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) + 0.115
 
     popt = recalibrate_parameters(X, Y)
     plot_data(X, Y, popt)
 
     print("The recalibrated parameters are:")
-    print(f"a = {popt[0]}")
-    print(f"b = {popt[1]}")
-    print(f"c = {popt[2]}")
-    print(f"d = {popt[3]}")
+    print(f"a = {popt[0]:.6f}")
+    print(f"b = {popt[1]:.6f}")
+    print(f"c = {popt[2]:.6f}")
+    print(f"d = {popt[3]:.6f}")
 
-    print("Testing the conversion functions:")
-    print(f"y_to_meters(1028) = {y_to_meters(1028)}")
-    print(f"y_to_meters(733) = {y_to_meters(733)}")
-    print(f"y_to_meters(387) = {y_to_meters(387)}")
-    print(f"meters_to_y(0.1) = {meters_to_y(0.1)}")
-    print(f"meters_to_y(10.1) = {meters_to_y(10.1)}")
-    print(f"meters_to_y(5000) = {meters_to_y(5000)}")
+    """Ensure that the height doesn't change the result."""
+    assert y_to_meters(1080, 1080) == y_to_meters(720, 720)
+    assert y_to_meters(561, 1080) == y_to_meters(374, 720)
+
+    """Ensure that y_to_meters and meters_to_y are inverse functions."""
+    assert meters_to_y(y_to_meters(1080, 1080), 1080) == 1080
+    assert meters_to_y(y_to_meters(720, 720), 720) == 720
+    assert meters_to_y(y_to_meters(561, 1080), 1080) == 561
