627cc30b35d556a0ea7821fe5ceafc4829648558
willtura <m.d.hooijer@st.hanze.nl>
2024-03-12 10:36:58 +0100

    Topdown converter relative

--- src/topdown_view/main.py
+++ src/topdown_view/main.py
@@ -1,30 +1,32 @@
 import cv2
 import numpy as np
-
+import time
 
 def cut_image(image, x, y, width, height):
     return image[y:y + height, x:x + width]
 
-
-def resize_image(image, target_width, target_height):
-    resized_image = cv2.resize(image, (target_width, target_height))
-    return resized_image
-
-
-def topdown(image):
-    """ a function to tranform the image to a top-down view of the road
-    Args:
-        image: the image to be transformed
-        creates a top down view of the road
-    """
+def topdown(image, image_width, image_height):
     if image is None:
         raise ValueError("Error: Unable to load image")
 
-    pts = np.array([[55, 900], [1841, 253], [2067, 253], [3861, 900]], dtype=np.float32)
-    ipm_pts = np.array([[780, 450], [800, 1100], [600, 1100], [620, 450]], dtype=np.float32)
+    DIVISOR_WIDTH = 3970
+    DIVISOR_HEIGHT = 1971
+
+    WIDTH_RATIO = 1 / DIVISOR_WIDTH
+    HEIGHT_RATIO = 1 / DIVISOR_HEIGHT
+
+    pts = np.array([[55 * WIDTH_RATIO * image_width, 900 * HEIGHT_RATIO * image_height],
+                    [1841 * WIDTH_RATIO * image_width, 253 * HEIGHT_RATIO * image_height],
+                    [2067 * WIDTH_RATIO * image_width, 253 * HEIGHT_RATIO * image_height],
+                    [3861 * WIDTH_RATIO * image_width, 900 * HEIGHT_RATIO * image_height]], dtype=np.float32)
+
+    ipm_pts = np.array([[600 * WIDTH_RATIO * image_width, 1100 * HEIGHT_RATIO * image_height],
+                        [620 * WIDTH_RATIO * image_width, 450 * HEIGHT_RATIO * image_height],
+                        [780 * WIDTH_RATIO * image_width, 450 * HEIGHT_RATIO * image_height],
+                        [800 * WIDTH_RATIO * image_width, 1100 * HEIGHT_RATIO * image_height]], dtype=np.float32)
+
     ipm_matrix = cv2.getPerspectiveTransform(pts, ipm_pts)
-    ipm = cv2.warpPerspective(image, ipm_matrix, (image.shape[1], image.shape[0]), flags=cv2.INTER_LINEAR)
-    ipm = cut_image(ipm, 300, 450, 800, 900)
-    ipm = cv2.rotate(ipm, cv2.ROTATE_90_CLOCKWISE)
-    ipm = cv2.rotate(ipm, cv2.ROTATE_90_CLOCKWISE)
+    ipm = cv2.warpPerspective(image, ipm_matrix, (image_width, image_height), flags=cv2.INTER_NEAREST)
+    ipm = cut_image(ipm, int(300 * WIDTH_RATIO * image_width), int(200 * HEIGHT_RATIO * image_height), int(800 * WIDTH_RATIO * image_width), int(900 * HEIGHT_RATIO * image_height))
+
     return ipm
