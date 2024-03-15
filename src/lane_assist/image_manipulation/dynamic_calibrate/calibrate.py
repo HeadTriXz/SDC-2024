6f42d42d97b1eaa1c96cf0845d11e36a8bc3ca0d
HeadTriXz <l.knol@outlook.com>
2024-03-15 14:09:55 +0100

    feat: dynamically calculate the bird view (wip)

---
+++ src/lane_assist/image_manipulation/dynamic_calibrate/calibrate.py
@@ -0,0 +1,126 @@
+import cv2
+import numpy as np
+
+from common.config import Calibration as Config
+from lane_assist.image_manipulation.dynamic_calibrate.utils import corners_to_grid, find_largest_rectangle, \
+    euclidean_distance
+
+
+def get_board_shape() -> tuple[int, int]:
+    """Get the shape of the ChArUco board.
+
+    :return: The shape of the ChArUco board.
+    """
+    return Config.BOARD_WIDTH, Config.BOARD_HEIGHT
+
+
+def initialize_charuco_detector() -> cv2.aruco.CharucoDetector:
+    """Initialize the ChArUco board and detector.
+
+    :return: The ChArUco detector.
+    """
+    dictionary = cv2.aruco.getPredefinedDictionary(Config.ARUCO_DICT)
+    detector_params = cv2.aruco.DetectorParameters()
+    charuco_params = cv2.aruco.CharucoParameters()
+
+    board_shape = get_board_shape()
+    board = cv2.aruco.CharucoBoard(board_shape, Config.SQUARE_LENGTH, Config.MARKER_LENGTH, dictionary)
+
+    return cv2.aruco.CharucoDetector(board, charuco_params, detector_params)
+
+
+def find_corners(image: np.ndarray, detector: cv2.aruco.CharucoDetector) -> tuple[np.ndarray, tuple[int, int]]:
+    """Find the ChArUco corners in an image.
+
+    :param image: The image to find the corners in.
+    :param detector: The ChArUco detector.
+    :return: The corners of the ChArUco board.
+    """
+    charuco_corners, charuco_ids, _, _ = detector.detectBoard(image)
+    if charuco_ids is None or len(charuco_ids) < 4:
+        raise ValueError("Not enough ChArUco corners found")
+
+    # Convert the 1D-array of corners and ids to a 2D-array
+    grid = corners_to_grid(charuco_corners, charuco_ids, get_board_shape())
+    binary_matrix = np.any(grid, axis=2).astype(np.uint8)
+
+    # Find the largest rectangle in the binary matrix
+    corner_indices = find_largest_rectangle(binary_matrix)
+    if corner_indices is None:
+        raise ValueError("No rectangle found in the ChArUco board")
+
+    rect_w = int(euclidean_distance(corner_indices[0], corner_indices[1]))
+    rect_h = int(euclidean_distance(corner_indices[1], corner_indices[2]))
+    shape = (rect_w, rect_h)
+
+    return grid[corner_indices[:, 0], corner_indices[:, 1]], shape
+
+
+def get_max_distance(corners: np.ndarray, shape: tuple[int, int]) -> float:
+    """Get the maximum distance from one square to another.
+
+    :param corners: The corners to calculate the distance from.
+    :param shape: The shape of the original image.
+    :return: The maximum distance from one square to another.
+    """
+    w, h = shape
+
+    dist_t = euclidean_distance(corners[0], corners[1]) / w
+    dist_b = euclidean_distance(corners[2], corners[3]) / w
+    dist_l = euclidean_distance(corners[0], corners[3]) / h
+    dist_r = euclidean_distance(corners[1], corners[2]) / h
+
+    return max(dist_t, dist_b, dist_l, dist_r)
+
+
+def remove_perspective(corners: np.ndarray, shape: tuple[int, int], length: int) -> np.ndarray:
+    """Remove perspective from a set of corners.
+
+    :param corners: The corners to remove perspective from.
+    :param shape: The shape of the original image.
+    :param length: The length of the squares of the ChArUco board.
+    :return: The corners with perspective removed.
+    """
+    w, h = shape
+    cx, cy = np.mean(corners, axis=0)
+
+    new_w = int(w * length)
+    new_h = int(h * length)
+
+    new_corners = np.array([
+        [cx - new_h / 2, cy + new_w / 2],
+        [cx - new_h / 2, cy - new_w / 2],
+        [cx + new_h / 2, cy - new_w / 2],
+        [cx + new_h / 2, cy + new_w / 2]
+    ])
+
+    if corners[0, 1] < corners[1, 1]:
+        new_corners = new_corners[::-1]
+
+    return new_corners
+
+
+def calibrate_cameras(batch: np.ndarray) -> np.ndarray:
+    """Calibrate multiple cameras using images of a ChArUco board.
+
+    :param batch: The images to use for calibration.
+    :return: The perspective matrices for each image.
+    """
+    detector = initialize_charuco_detector()
+    all_corners = np.empty((len(batch), 4, 2), dtype=np.float32)
+    all_shapes = np.empty((len(batch), 2), dtype=np.int32)
+
+    for i, image in enumerate(batch):
+        all_corners[i], all_shapes[i] = find_corners(image, detector)
+
+    min_size = float("inf")
+    for src_corners, shape in zip(all_corners, all_shapes):
+        max_dist = get_max_distance(src_corners, shape)
+        min_size = min(min_size, max_dist)
+
+    dst_matrices = np.empty((len(batch), 3, 3), dtype=np.float32)
+    for i, (src_corners, shape) in enumerate(zip(all_corners, all_shapes)):
+        dst_corners = remove_perspective(src_corners, shape, min_size)
+        dst_matrices[i] = cv2.findHomography(src_corners, dst_corners)[0]
+
+    return dst_matrices
---
+++ src/lane_assist/image_manipulation/dynamic_calibrate/utils.py
@@ -0,0 +1,111 @@
+from typing import Optional
+
+import cv2
+import numpy as np
+
+Coordinate = tuple[int, int]
+Corners = tuple[Coordinate, Coordinate, Coordinate, Coordinate]
+
+
+def find_largest_rectangle(matrix: np.ndarray) -> Optional[np.ndarray]:
+    """Find the largest rectangle in a binary matrix.
+
+    :param matrix: The binary matrix.
+    :return: The corners of the largest rectangle (tl, tr, br, bl).
+    """
+    if not np.any(matrix):
+        return None
+
+    max_area = 0
+    top_left = (0, 0)
+    bottom_right = (0, 0)
+
+    rows, cols = matrix.shape[:2]
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i, j] == 0:
+                continue
+
+            for x in range(i, rows):
+                for y in range(j, cols):
+                    if matrix[x][y] == 1 and matrix[i][y] == 1 and matrix[x][j] == 1:
+                        area = (x - i + 1) * (y - j + 1)
+                        if area > max_area:
+                            max_area = area
+                            top_left = (i, j)
+                            bottom_right = (x, y)
+
+    top_right = (top_left[0], bottom_right[1])
+    bottom_left = (bottom_right[0], top_left[1])
+
+    return np.array([top_left, top_right, bottom_right, bottom_left])
+
+
+def euclidean_distance(p1: np.ndarray | Coordinate, p2: np.ndarray | Coordinate) -> float:
+    """Calculate the Euclidean distance between two points.
+
+    :param p1: The first point.
+    :param p2: The second point.
+    :return: The Euclidean distance between the two points.
+    """
+    return np.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+
+
+def corners_to_grid(corners: np.ndarray, ids: np.ndarray, shape: tuple[int, int]) -> np.ndarray:
+    """Convert corners and ids to a grid.
+
+    :param corners: An array of corners.
+    :param ids: An array of ids for each corner.
+    :param shape: The shape of the board (w, h).
+    :return: The grid of corners.
+    """
+    w, h = np.subtract(shape, 1)
+    grid = np.zeros((h, w, 2), dtype=np.float32)
+
+    for corner, corner_id in zip(corners[:, 0], ids[:, 0]):
+        col = corner_id % w
+        row = corner_id // w
+
+        grid[row, col] = corner
+
+    return grid
+
+
+def adjust_perspective(matrix: np.ndarray, shape: tuple[int, int]) -> tuple[np.ndarray, int, int]:
+    """Adjust the perspective matrix to fit within the new image.
+
+    :param matrix: The perspective matrix.
+    :param shape: The shape of the old image.
+    :return: The adjusted perspective matrix and the new shape of the image.
+    """
+    h, w = shape
+    src_points = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype=np.float32).reshape(-1, 1, 2)
+    dst_points = cv2.perspectiveTransform(src_points, matrix)
+
+    min_x = np.amin(dst_points[:, 0, 0])
+    min_y = np.amin(dst_points[:, 0, 1])
+    max_x = np.amax(dst_points[:, 0, 0])
+    max_y = np.amax(dst_points[:, 0, 1])
+
+    new_w = int(max_x - min_x)
+    new_h = int(max_y - min_y)
+
+    adjusted_matrix = np.array([[1, 0, -min_x if min_x < 0 else 0],
+                                [0, 1, -min_y if min_y < 0 else 0],
+                                [0, 0, 1]])
+
+    adjusted_matrix = np.dot(adjusted_matrix, matrix)
+
+    return adjusted_matrix, new_w, new_h
+
+
+def scale_matrix(matrix: np.ndarray, scale_factor: float) -> np.ndarray:
+    """Scale the perspective matrix.
+
+    :param matrix: The perspective matrix.
+    :param scale_factor: The scale factor.
+    :return: The scaled perspective matrix.
+    """
+    scaled_matrix = np.array([[scale_factor, 0, 0], [0, scale_factor, 0], [0, 0, 1]], dtype=np.float32)
+    return np.dot(scaled_matrix, matrix)
