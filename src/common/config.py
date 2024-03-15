ceb52f419a5dbaca9e391a18b4b1131b06e74f4f
HeadTriXz <l.knol@outlook.com>
2024-03-15 14:09:55 +0100

    feat: dynamically calculate the bird view (wip)

--- src/common/config.py
+++ src/common/config.py
@@ -1,5 +1,9 @@
+import cv2
+
 from common.constants import SpeedMode
 
+
+# Speed Controller
 speed_mode: SpeedMode = SpeedMode.SLOW
 speed_mode_to_speed = {
     SpeedMode.SLOW:      25,
@@ -9,3 +13,14 @@ speed_mode_to_speed = {
 }
 
 speed = speed_mode_to_speed[speed_mode]
+
+class Calibration:
+    """The calibration settings for the bird's eye view perspective matrix."""
+
+    ARUCO_DICT = cv2.aruco.DICT_4X4_50
+    BOARD_HEIGHT = 8
+    BOARD_WIDTH = 6
+    MAX_IMAGE_WIDTH = 1920
+    MAX_IMAGE_HEIGHT = 1080
+    MARKER_LENGTH = 0.070
+    SQUARE_LENGTH = 0.094
---
+++ src/lane_assist/image_manipulation/dynamic_calibrate/birdview.py
@@ -0,0 +1,45 @@
+import time
+
+import cv2
+import numpy as np
+
+from lane_assist.image_manipulation.dynamic_calibrate.calibrate import calibrate_cameras
+from lane_assist.image_manipulation.dynamic_calibrate.utils import adjust_perspective, scale_matrix
+
+
+def warp_image(image: np.ndarray, matrix: np.ndarray) -> np.ndarray:
+    """Warp an image using a perspective matrix.
+
+    :param image: The image to warp.
+    :param matrix: The perspective matrix.
+    :return: The warped image.
+    """
+    matrix, width, height = adjust_perspective(matrix, (image.shape[0], image.shape[1]))
+
+    # scale_factor = min(Config.MAX_IMAGE_WIDTH / width, Config.MAX_IMAGE_HEIGHT / height)
+    # matrix = scale_matrix(matrix, scale_factor)
+    # new_height = int(height * scale_factor)
+    # new_width = int(width * scale_factor)
+
+    return cv2.warpPerspective(image, matrix, (width, height))
+
+
+if __name__ == "__main__":
+    image1 = cv2.imread("images/fake1.png")
+    image2 = cv2.imread("images/fake2.png")
+
+    image1 = cv2.resize(image1, (1280, 720))[300:]
+    image2 = cv2.resize(image2, (1280, 720))[300:]
+
+    images = np.array([image1, image2])
+    matrices = calibrate_cameras(images)
+
+    start = time.perf_counter()
+    for _ in range(1000):
+        for img, matrix in zip(images, matrices):
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            result = warp_image(img, matrix)
+    end = time.perf_counter()
+
+    print(f"Time: {end - start:.4f} seconds")
+    print(f"FPS: {1000 / (end - start):.2f}")
