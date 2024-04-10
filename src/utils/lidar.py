f0012d8b39573a92aa22031a34ab7497bf6852cf
Willtura <m.d.hooijer@st.hanze.nl>
2024-04-10 15:04:00 +0200

    Lidar v2

--- src/utils/lidar.py
+++ src/utils/lidar.py
@@ -6,67 +6,86 @@ PORT_NAME = "COM4"
 lidar = RPLidar(None, PORT_NAME, timeout=3)
 max_distance = 0
 
-def find_obstacle_distance(data :list[int], anglemin: int, anglemax:int) -> int:
-    """A function that finds the distance to the closest obstacle in a certain angle range.
+class Lidar:
+    """A class to read data from the lidar and process the data from it."""
 
-    :param data: the data from the lidar
-    :param anglemin: the minimum angle to check
-    :param anglemax: the maximum angle to check
+    def __init__(self, port_name: str) -> None:
+        """Initializes the lidar.
 
-    :return: the distance to the closest obstacle
-    """
-    point = 15000
-    for i in range(anglemin, anglemax):
-        if point > data[i] > 500:
-            point = data[i]
-    return point
-def right_free(data: list[int]) -> bool:
-    """A function that checks if the right side of the car is free.
+        :param port_name: the name of the port
+        """
+        self.port_name = port_name
+        self.lidar = RPLidar(None, self.port_name, timeout=3)
+        self.max_distance = 0
+        self.scan_data = [0]*360
 
-    :param data: the data from the lidar
-    :return: True if the right side is free, False otherwise
-    """
-    if min(data[20:120]) < 2500:
-        for i in range(20, 120):
-            if data[i] < 2500 and(data[i+1] < 2500 or data[i-1] < 2500):
-                return False
-    return True
+    def find_obstacle_distance(self, data: list[int], anglemin: int, anglemax: int) -> int:
+        """A function that finds the distance to the closest obstacle in a certain angle range.
 
-def free_side(data: list[int], anglemin: int, anglemax: int) -> bool:
-    """A function that checks if the side between anglemin and anglemax of the car is free.
+        :param data: the data from the lidar
+        :param anglemin: the minimum angle to check
+        :param anglemax: the maximum angle to check
 
-    :param data: the data from the lidar
-    :param side: the side to check, either "right" or "left"
-    :return: True if the side is free, False otherwise
-    """
-    return all(not (data[i] < 2500 and (data[i + 1] < 2500 or data[i - 1] < 2500)) for i in range(anglemin, anglemax))
+        :return: the distance to the closest obstacle
+        """
+        point = 15000
+        for i in range(anglemin, anglemax):
+            if point > data[i] > 500:
+                point = data[i]
+        return point
 
-def data_processing(data: list[int]) -> list[int]:
-    """A function that processes the data from the lidar.
+    def right_free(self, data: list[int]) -> bool:
+        """A function that checks if the right side of the car is free.
 
-    :param data: the data from the lidar
+        :param data: the data from the lidar
+        :return: True if the right side is free, False otherwise
+        """
+        if min(data[20:120]) < 2500:
+            for i in range(20, 120):
+                if data[i] < 2500 and(data[i+1] < 2500 or data[i-1] < 2500):
+                    return False
+        return True
 
-    :return: the processed data
-    """
-    distances = np.array(data)
-    newdistances = distances.copy()
-    for i in range(len(distances) - 1):
-        if distances[i] < 500:
-            newdistances[i] = 15000
+    def free_side(self, data: list[int], anglemin: int, anglemax: int) -> bool:
+        """A function that checks if the side between anglemin and anglemax of the car is free.
 
-        if np.abs(distances[i] - distances[i + 1]) > 1500:
-            newdistances[i] = np.nan
-    return newdistances
+        :param data: the data from the lidar
+        :param side: the side to check, either "right" or "left"
+        :return: True if the side is free, False otherwise
+        """
+        return all(
+            not (
+                    data[i] < 2500 and (data[i + 1] < 2500 or data[i - 1] < 2500)
+            ) for i in range(anglemin, anglemax)
+        )
+    def data_processing(self, data: list[int]) -> list[int]:
+        """A function that processes the data from the lidar.
 
-try:
-    scan_data = [0]*360
-    for scan in lidar.iter_scans():
-        scan_data = [0]*360
-        for (_, angle, distance) in scan:
-            scan_data[min([359, floor(angle)])] = distance
-        processed_data = data_processing(scan_data)
+        :param data: the data from the lidar
 
-except KeyboardInterrupt:
-    lidar.stop()
-lidar.stop()
-lidar.disconnect()
+        :return: the processed data
+        """
+        distances = np.array(data)
+        newdistances = distances.copy()
+        for i in range(len(distances) - 1):
+            if distances[i] < 500:
+                newdistances[i] = 15000
+
+            if np.abs(distances[i] - distances[i + 1]) > 1500:
+                newdistances[i] = np.nan
+        return newdistances
+
+    def start(self) -> None:
+        """Start the lidar."""
+        try:
+            for scan in self.lidar.iter_scans():
+                self.scan_data = [0]*360
+                for (_, angle, distance) in scan:
+                    self.scan_data[min([359, floor(angle)])] = distance
+        except KeyboardInterrupt:
+            self.lidar.stop()
+    def stop(self) -> None:
+        """Stop the lidar."""
+        self.lidar.stop()
+        self.lidar.disconnect()
+
