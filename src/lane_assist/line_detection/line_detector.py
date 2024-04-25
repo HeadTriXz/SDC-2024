- fixed overflow of indexes for the window search  - added test images  - fixed detection of stoplines  - fixed styles  - improved filtering

src/lane_assist/line_detection/line_detector.py
@@ -8,7 +8,9 @@ from lane_assist.line_detection.line import Line, LineType
 from lane_assist.line_detection.window import Window
 from lane_assist.line_detection.window_search import window_search
 from typing import Any
+from lane_assist.preprocessing.image_filters import basic_filter
 from utils.calibration_data import CalibrationData
+
 
 def filter_lines(lines: list[Line], starting_point: int) -> list[Line]:
     """Get the lines between the solid lines closest to each side of the starting point.
@@ -47,7 +49,8 @@ def get_lines(image: np.ndarray, calibration: CalibrationData) -> list[Line]:
     cv2.threshold(image, config.image_manipulation.white_threshold, 255, cv2.THRESH_BINARY, image)
 
     # Filter the image. This is done in place and will be used to remove zebra crossings.
-    # basic_filter(image)  # noqa: ERA001 might be needed in real life
+    if config.lane_assist.line_detection.filtering.active:
+        basic_filter(image, calibration)
 
     # create histogram to find the start of the lines
     pixels = image[image.shape[0] // 2:, :]
@@ -114,8 +117,8 @@ def filter_stoplines(lines: list[Line], window_height: int, minimum_points: int,
     filtered_lines = []
     for line in lines:
         gaps = np.diff(line.points[:, 0])
-        start, stop = __longest_sequence(gaps, lambda x: x == -window_height)
-        if stop - start > minimum_points and stop - start < max_points:
+        start, stop = __longest_sequence(gaps, lambda x: window_height + 2 > -x > window_height - 2)
+        if minimum_points < stop - start < max_points:
             filtered_lines.append(Line(line.points[start:stop], line_type=LineType.STOP))
 
     return filtered_lines
@@ -157,7 +160,6 @@ def __get_lines(
     window_count = image.shape[0] // window_height
 
     peaks = scipy.signal.find_peaks(histogram, height=std, distance=window_width * 2)[0]
-
     windows = [Window(center, image.shape[0], window_width // 2, window_count) for center in peaks]
     lines = window_search(image, window_count, windows, image.shape[0] // window_count, stopline)
     return lines, window_height

src/lane_assist/line_detection/window_search.py
@@ -8,7 +8,6 @@ from lane_assist.line_detection.line import Line, LineType
 from lane_assist.line_detection.window import Window
 
 
-
 def window_search(
         filtered_img: np.ndarray, window_count: int, windows: Iterable[Window], window_height: int,
         stopline: bool = False
@@ -33,10 +32,10 @@ def window_search(
 
         for window in running_windows:
             # Get the new sides of the window.
-            top = window.y - window_height
-            bottom = window.y
-            left = window.x - int(window.margin)
-            right = window.x + int(window.margin)
+            top = min(max(window.y - window_height, 0), filtered_img.shape[0])
+            bottom = min(max(window.y, 0), filtered_img.shape[0])
+            left = min(max(window.x - int(window.margin), 0), filtered_img.shape[1])
+            right = min(max(window.x + int(window.margin), 0), filtered_img.shape[1])
 
             non_zero_count = np.sum(filtered_img[top:bottom, left:right]) // 255
             if non_zero_count < config.lane_assist.line_detection.pixels_in_window:

src/lane_assist/preprocessing/image_filters.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy
 
 from config import config
+from utils.calibration_data import CalibrationData
 
 
 @dataclasses.dataclass
@@ -24,7 +25,7 @@ class HistogramPeak:
     right: int
 
 
-def basic_filter(img: np.ndarray) -> tuple[np.ndarray, list[HistogramPeak]]:
+def basic_filter(img: np.ndarray, calibration: CalibrationData) -> tuple[np.ndarray, list[HistogramPeak]]:
     """Filter the image based on the axis.
 
     :param img: The image to filter.
@@ -34,9 +35,11 @@ def basic_filter(img: np.ndarray) -> tuple[np.ndarray, list[HistogramPeak]]:
     pixels = img[:, third : 2 * third]
 
     histogram = np.sum(pixels, axis=1)
-
+    width = calibration.pixels_per_meter * 0.5
     peaks = scipy.signal.find_peaks(
-        histogram, height=config.lane_assist.line_detection.thresholds.zebra_crossing, distance=40
+        histogram,
+        height=config.lane_assist.line_detection.thresholds.zebra_crossing,
+        width=width
     )[0]
 
     widths, _, lefts, rights = scipy.signal.peak_widths(
@@ -50,11 +53,9 @@ def basic_filter(img: np.ndarray) -> tuple[np.ndarray, list[HistogramPeak]]:
     if len(peaks) == 0:
         return img, []
 
-    std = np.std(histogram)
     for peak in histogram_peaks:
-        if peak.width > std:
-            img[int(peak.left) : int(peak.right)] = 0
-            img[int(peak.left) : int(peak.right)] = 0
+        img[int(peak.left) : int(peak.right)] = 0
+        img[int(peak.left) : int(peak.right)] = 0
 
     return img, histogram_peaks
 