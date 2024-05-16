88821be5622dde5192ecce7e02d5050d46e921ae
willtura <m.d.hooijer@st.hanze.nl>
2024-05-16 11:04:17 +0200

    fixed lidar.py

--- src/utils/lidar.py
+++ src/utils/lidar.py
@@ -52,6 +52,7 @@ class Lidar:
         :param angle_min: The minimum angle to check. (180 is the front of the car)
         :param angle_max: The maximum angle to check. (180 is the front of the car)
         :param distance: The minimum distance to check.
+        :return: Whether the side is free.
         """
         return self.find_obstacle_distance(angle_min, angle_max) > distance
 
