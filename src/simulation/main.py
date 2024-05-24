0958baeefc1c3f407e0c3b7622ced283262d0b5b
HeadTriXz <l.knol@outlook.com>
2024-05-24 21:03:58 +0200

    feat: make the simulator look more realistic

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -36,24 +36,37 @@ def start_simulator() -> None:
             responses = client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)])
             response = responses[0]
 
-            img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8)
-            img_rgb = img1d.reshape(response.height, response.width, 3)
-            img_rotated = cv2.rotate(img_rgb, cv2.ROTATE_90_COUNTERCLOCKWISE)
-            if img_rotated is None:
+            flat_image = np.frombuffer(response.image_data_uint8, dtype=np.uint8)
+            rgb_image = flat_image.reshape(response.height, response.width, 3)
+            rot_image = cv2.rotate(rgb_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            if rot_image is None:
                 continue
 
-            grayscale = cv2.cvtColor(img_rotated[:-42, :], cv2.COLOR_BGR2GRAY)
-            grayscale = cv2.threshold(grayscale, config["preprocessing"]["white_threshold"], 255, cv2.THRESH_BINARY)[1]
+            grayscale = cv2.cvtColor(rot_image[:-20, :], cv2.COLOR_BGR2GRAY)
+            thresholded = cv2.threshold(grayscale, config["preprocessing"]["white_threshold"], 255, cv2.THRESH_BINARY)[1]
 
-            cx, cy = grayscale.shape[1] // 2, grayscale.shape[0]
+            # Crop the image to the same size as real-world images.
+            cx, cy = thresholded.shape[1] // 2, thresholded.shape[0]
 
-            grayscale = grayscale[cy - my:cy + my, cx - mx:cx + mx]
-            grayscale = cv2.resize(grayscale, (width, height))
+            cropped = thresholded[cy - my:cy + my, cx - mx:cx + mx]
+            resized = cv2.resize(cropped, (width, height))
+
+            # Add information loss; this is the same as with real cameras.
+            front_view = cv2.warpPerspective(
+                resized,
+                calibration.topdown_matrix,
+                calibration.stitched_shape,
+                flags=cv2.WARP_INVERSE_MAP | cv2.INTER_NEAREST
+            )
+
+            top_view = cv2.warpPerspective(
+                front_view, calibration.topdown_matrix, calibration.output_shape, flags=cv2.INTER_NEAREST
+            )
 
             if config["telemetry"]["enabled"]:
-                telemetry.websocket_handler.send_image("topdown", grayscale)
+                telemetry.websocket_handler.send_image("topdown", top_view)
 
-            yield grayscale
+            yield top_view
 
     can_controller = SimCanController()
     speed_controller = SpeedController(can_controller)
