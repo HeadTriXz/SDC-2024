8c38a8c372176e603640df7b3424689961b29140
Michel Gerding <michelgerding@gmail.com>
2024-05-25 21:37:46 +0200

    added alterantive algo for image filtering

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
+import cv2
 import logging
-import os
 
-from src.driving.kart import Kart
-from src.telemetry.logging_handler import LoggingHandler
+from src.calibration.data import CalibrationData
+from src.lane_assist.preprocessing.image_filters import morphex_filter
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, handlers=[LoggingHandler()])
+    image_names = [f"./data/images/topdown/{i}.jpg" for i in range(1, 22)]
+    images = [cv2.imread(file, cv2.IMREAD_GRAYSCALE) for file in image_names]
 
-    if "ENVIRONMENT" in os.environ and os.environ["ENVIRONMENT"] == "simulator":
-        from src.simulation.main import start_simulator
+    calibration = CalibrationData.load("./data/calibration/latest.npz")
 
-        start_simulator()
-    else:
-        Kart().start()
+    for i, image in enumerate(images):
+        logging.debug("showing image %s", image_names[i])
+        cv2.imshow("original", image)
+
+        # filter the image
+        filter_mask = cv2.bitwise_not(morphex_filter(image, calibration))
+        filtered_image = cv2.bitwise_and(image, filter_mask)
+        cv2.imshow("mask", filter_mask)
+        cv2.imshow("filtered", filtered_image)
+
+        while cv2.waitKey(1) != ord("q"):
+            pass
