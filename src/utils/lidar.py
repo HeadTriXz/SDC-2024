5b37e87458142b83477fc5cf4fe2a2b1ea1980b5
HeadTriXz <l.knol@outlook.com>
2024-06-04 09:16:38 +0200

    fix: make lidar safer

--- src/utils/lidar.py
+++ src/utils/lidar.py
@@ -17,21 +17,21 @@ class Lidar:
     Attributes
     ----------
         lidar: The lidar object.
-        running: Whether the lidar is running.
         scan_data: The data from the lidar.
         thread: The thread that captures the data from the lidar.
 
     """
 
     lidar: RPLidar
-    running: bool = False
     scan_data: np.ndarray
     thread: Thread
 
     def __init__(self) -> None:
         """Initializes the lidar."""
-        self.lidar = RPLidar(config["lidar"]["port_name"], timeout=5)
-        self.thread = Thread(target=self.capture, daemon=True)
+        self.lidar = RPLidar(config["lidar"]["port_name"], timeout=3)
+        self.lidar.stop_motor()
+
+        self.thread = Thread(target=self.__listen, daemon=True)
         self.scan_data = np.full(360, np.inf)
 
     def find_obstacle_distance(self, angle_min: int, angle_max: int) -> int:
@@ -56,30 +56,40 @@ class Lidar:
         """
         return self.find_obstacle_distance(angle_min, angle_max) > distance
 
-    def capture(self) -> None:
-        """A function that captures the data from the lidar and filters it."""
-        for scan in self.lidar.iter_scans():
-            if not self.running:
-                return
-
-            for _, angle, distance in scan:
-                angle = int(angle % 360)
-                if distance < config["lidar"]["min_distance"]:
-                    self.scan_data[floor(angle)] = np.inf
-                    continue
-
-                self.scan_data[floor(angle)] = distance
-
     def start(self) -> None:
         """Start the lidar."""
-        self.running = True
-        self.lidar.reset()
         if not self.thread.is_alive():
             self.thread.start()
 
     def stop(self) -> None:
         """Stop the lidar."""
-        self.running = False
+        self.thread.join()
+        self.lidar.stop()
+        self.lidar.stop_motor()
+        self.lidar.disconnect()
+
+    def __capture(self) -> None:
+        """A function that captures the data from the lidar and filters it."""
+        self.lidar.start_motor()
+        for scan in self.lidar.iter_scans():
+            for _, angle, distance in scan:
+                angle = min(359, floor(angle))
+                if distance < config["lidar"]["min_distance"]:
+                    self.scan_data[angle] = np.inf
+                    continue
+
+                self.scan_data[angle] = distance
+
+    def __listen(self) -> None:
+        """Listen for data from the lidar sensor."""
+        while True:
+            try:
+                self.__capture()
+            except Exception as e:
+                logging.error("Failed to capture data from the lidar: %s", e)
+
+                self.lidar.stop()
+                self.lidar.stop_motor()
 
     @classmethod
     def safe_init(cls) -> Optional["Lidar"]:
